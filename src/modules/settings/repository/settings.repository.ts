e7a230aea4f9b0dc2293e944027f88e318f539d2
aghyad <zenlogie@gmail.com>
2026-08-18 18:47:36 +0200

    chore(Package): configure pathing alias and remove tests

--- src/modules/settings/repository/settings.repository.ts
+++ src/modules/settings/repository/settings.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Settings } from '../domain/settings.model';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Injectable()
 export class SettingsRepository {
